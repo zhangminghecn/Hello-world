de717da4d47e59d5c0f835a120bdc4d03a162c96
lixiaofeng <lixiaofeng@feinno.com>
2016-02-24 10:53:11 +0800

    add files

---
+++ Helloworld-FromGithub/Helloworld-FromGithub/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Helloworld-FromGithub
+//
+//  Created by monkey on 16/2/24.
+//  Copyright © 2016年 monkey. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
